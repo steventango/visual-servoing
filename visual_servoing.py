6f3ddefde86a0672bf9ed6ddbfc74b39c9389d6c
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-05-04 15:40:02 -0600

    major refactor

--- visual_servoing.py
+++ visual_servoing.py
@@ -13,18 +13,19 @@ POINT_ORIGIN = np.array([
     0,
     1
 ]).T
-POINT_TARGET = np.array([
+POINT_TARGET = np.array([[
     -0.3,
     0.3,
     0.7,
     1
-]).T
+]]).T
 THETA = np.array([0, -np.pi/4, np.pi/2])
 LENGTH = np.array([0, 0.55, 0.30])
 TRAJECTORY_Z_MIDPOINT = 0.6
 
 PROJECTION_1 = camera_matrix([np.pi/2, 0, 0], [0, -1, 0.5])
 PROJECTION_2 = camera_matrix([np.pi/2, 0, -np.pi/2], [-1, 0, 0.5])
+PROJECTIONS = np.stack((PROJECTION_1, PROJECTION_2))
 
 
 def forward_kinematics(theta: np.ndarray):
@@ -43,7 +44,7 @@ def forward_kinematics(theta: np.ndarray):
     return X
 
 
-def render(X, p, p2, img_points_left, img_points_right):
+def render(X, target, POINT_TARGET, img_X, img_target, img_TARGET):
     fig = plt.figure(figsize=(12, 9))
     ax_3d = fig.add_subplot(2, 2, (1, 2), projection='3d')
     ax_image_left = fig.add_subplot(2, 2, 3)
@@ -51,13 +52,12 @@ def render(X, p, p2, img_points_left, img_points_right):
     axs = (ax_3d, ax_image_left, ax_image_right)
     ax_3d, ax_image_left, ax_image_right = axs
     ax_image_left.sharex(ax_image_right)
-    render_image(img_points_left, ax_image_left)
-    render_image(img_points_right, ax_image_right)
+    render_images(img_X, img_target, img_TARGET, axs[1:])
 
     ax_3d.plot(X[0, :], X[1, :], X[2, :], 'o-', label='Robot')
-    ax_3d.plot(p[0], p[1], p[2], 'o', label='Target')
-    ax_3d.plot(p2[0], p2[1], p2[2], 'o', label='Trajectory Target')
-    ax_3d.plot(X[0, -1], X[1, -1], X[2, -1], 'o-', label='End Effector')
+    ax_3d.plot(POINT_TARGET[0], POINT_TARGET[1], POINT_TARGET[2], 'o', label='Target')
+    ax_3d.plot(target[0], target[1], target[2], 'o', label='Trajectory Target')
+    ax_3d.plot(X[0, -1], X[1, -1], X[2, -1], 'o', label='End Effector')
     ax_3d.legend(loc='upper right')
     ax_3d.set_xlabel('X')
     ax_3d.set_xlim(-1, 1)
@@ -68,16 +68,18 @@ def render(X, p, p2, img_points_left, img_points_right):
     ax_3d.set_aspect('equal')
 
 
-def render_image(img_points, ax_image):
-    ax_image.plot(img_points[0, :-2], img_points[1, :-2], 'o-', label='Robot')
-    ax_image.plot(img_points[0, -1], img_points[1, -1], 'o', label='Target')
-    ax_image.plot(img_points[0, -2], img_points[1, -2], 'o', label='Trajectory Target')
-    ax_image.plot(img_points[0, -3], img_points[1, -3], 'o-', label='End Effector')
-    ax_image.set_aspect('equal')
-    ax_image.set_xlabel('u')
-    ax_image.set_ylabel('v')
-    ax_image.set_xlim(-1, 1)
-    ax_image.set_ylim(-1, 1)
+def render_images(img_X, img_target, img_TARGET, axs):
+    img_end_effector = img_X[:, :, -1]
+    for i, ax in enumerate(axs):
+        ax.plot(img_X[i, 0], img_X[i, 1], 'o-', label='Robot')
+        ax.plot(img_TARGET[i, 0], img_TARGET[i, 1], 'o', label='Target')
+        ax.plot(img_target[i, 0], img_target[i, 1], 'o', label='Trajectory Target')
+        ax.plot(img_end_effector[i, 0], img_end_effector[i, 1], 'o', label='End Effector')
+        ax.set_aspect('equal')
+        ax.set_xlabel('u')
+        ax.set_ylabel('v')
+        ax.set_xlim(-1, 1)
+        ax.set_ylim(-1, 1)
 
 
 def img_error(img_points):
@@ -90,17 +92,24 @@ def init_jacobian_central_differences(theta, target, delta=0.15):
     J = np.zeros((4, 3))
     for i in range(3):
         theta[i] += delta
-        _, img_points_left, img_points_right = get_points(theta, target)
-        e_r = get_error(img_points_left, img_points_right)
+        X = forward_kinematics(theta)
+        img_X = project_points(PROJECTIONS, X)
+        img_end_effector = img_X[:, :, -1]
+        img_target = project_points(PROJECTIONS, target)
+        e_r = get_error(img_end_effector, img_target)
         theta[i] -= 2 * delta
-        _, img_points_left, img_points_right = get_points(theta, target)
-        e_l = get_error(img_points_left, img_points_right)
+        X = forward_kinematics(theta)
+        img_X = project_points(PROJECTIONS, X)
+        img_end_effector = img_X[:, :, -1]
+        img_target = project_points(PROJECTIONS, target)
+        e_l = get_error(img_end_effector, img_target)
         J[:, i] = (e_r - e_l) / (2 * delta)
         theta[i] += delta
     return J
 
 
 def generate_trajectory(start, stop, num=10):
+    stop = np.squeeze(stop)
     trajectory = np.linspace(start, stop, num).T
     # fit a quadratic polynomial on z
     poly = np.polyfit([-1, 0, 1], [start[2], TRAJECTORY_Z_MIDPOINT, stop[2]], 2)
@@ -110,26 +119,36 @@ def generate_trajectory(start, stop, num=10):
 
 def main():
     theta = THETA
-    X, img_points_left, img_points_right = get_points(theta, POINT_TARGET)
-    render(X, POINT_TARGET, POINT_TARGET, img_points_left, img_points_right)
+    X = forward_kinematics(theta)
+    img_X = project_points(PROJECTIONS, X)
+    img_TARGET = project_points(PROJECTIONS, POINT_TARGET)
+    render(X, POINT_TARGET, POINT_TARGET, img_X, img_TARGET, img_TARGET)
     plt.show()
     plt.close()
 
     trajectory = generate_trajectory(X[:, -1], POINT_TARGET)
     for target in trajectory.T:
-        X, img_points_left, img_points_right, theta = broydens_method(theta, target)
+        theta = broydens_method(theta, target)
 
-    render(X, POINT_TARGET, POINT_TARGET, img_points_left, img_points_right)
+    X = forward_kinematics(theta)
+    img_X = project_points(PROJECTIONS, X)
+    img_target = project_points(PROJECTIONS, target)
+    img_TARGET = project_points(PROJECTIONS, POINT_TARGET)
+    render(X, target, POINT_TARGET, img_X, img_target, img_TARGET)
     plt.show()
 
 
 def broydens_method(theta, target):
     B = init_jacobian_central_differences(theta, target)
+    img_TARGET = project_points(PROJECTIONS, POINT_TARGET)
     e_prev = None
     s_prev = None
     for _ in range(N):
-        X, img_points_left, img_points_right = get_points(theta, target)
-        e = get_error(img_points_left, img_points_right)
+        X = forward_kinematics(theta)
+        img_X = project_points(PROJECTIONS, X)
+        img_end_effector = img_X[:, :, -1]
+        img_target = project_points(PROJECTIONS, target)
+        e = get_error(img_end_effector, img_target)
         if la.norm(e) < 1e-3:
             plt.close()
             break
@@ -138,36 +157,26 @@ def broydens_method(theta, target):
         if e_prev is not None:
             y = e - e_prev
             B += beta * np.outer(y - B @ s_prev, s_prev.T) / (s_prev.T @ s_prev)
-            # J = init_jacobian_central_differences(theta, target)
-            # print(f"J: {J}")
-            # print(f"B: {B}")
-            # B = init_jacobian_central_differences(theta)
         e_prev = e
         s_prev = s
-        render(X, POINT_TARGET, target, img_points_left, img_points_right)
+        render(X, target, POINT_TARGET, img_X, img_target, img_TARGET)
         plt.pause(0.01)
         plt.close()
-    return X, img_points_left, img_points_right, theta
+    return theta
 
 
-def project_points(P, points):
-    image = P @ points
-    image /= image[2, :]
+def project_points(Ps, points):
+    if len(points.shape) < 2:
+        points = points.reshape((-1, 1))
+    image = Ps @ points
+    image = np.transpose(image, (1, 0, 2))
+    image = image / image[2, :, :]
+    image = np.transpose(image, (1, 0, 2))
     return image
 
 
-def get_points(theta, target):
-    X = forward_kinematics(theta)
-    points = np.column_stack((X, POINT_TARGET, target))
-    img_points_left = project_points(PROJECTION_1, points)
-    img_points_right = project_points(PROJECTION_2, points)
-    return X, img_points_left, img_points_right
-
-
-def get_error(img_points_left, img_points_right):
-    error_left = img_error(img_points_left)
-    error_right = img_error(img_points_right)
-    error = np.hstack((error_left, error_right))
+def get_error(img_end_effector, img_target):
+    error = (img_end_effector[:, :2] - img_target.squeeze()[:, :2]).flatten()
     return error
 
 
