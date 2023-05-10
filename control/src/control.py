b3ece1aad561a434220815b016c14c80ea8e1b43
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-05-10 16:15:21 -0600

    Changes to base class

--- control/src/control.py
+++ control/src/control.py
@@ -1,6 +1,13 @@
+import numpy as np
+from wam import WAM
+
 class ControlMethod:
     def __init__(self, args):
         pass
-    
-    def get_action(self, state):
-      raise NotImplementedError() 
+
+    def initialize(self, wam: WAM, control_node):
+        if self.initialized:
+            return
+
+    def get_action(self, state: np.ndarray, position: np.ndarray):
+      raise NotImplementedError()
