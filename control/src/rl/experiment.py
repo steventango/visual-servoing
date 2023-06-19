fd34c2d512a9bb8690121e59b25f6c4ce781eeec
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-06-19 15:24:16 -0600

    DOF experiment

--- control/src/rl/experiment.py
+++ control/src/rl/experiment.py
@@ -1,47 +1,71 @@
+import itertools
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from copy import deepcopy
 from pathlib import Path
 
 import numpy as np
+from data import combine_data
 from tqdm import tqdm
 from train import parse_args, train
 
 
 def main():
+    experiment = "DOFs"
+    model_path = Path(f"experiments/{experiment}/models")
+    eval_log_path = Path(f"experiments/{experiment}/data")
+    tensorboard_log_path = Path(f"experiments/{experiment}/logs")
+    repeats = 1
     args = parse_args(
         [
-            "control/src/rl/models/experiment",
+            "--model_path",
+            str(model_path),
+            "--eval_log_path",
+            str(eval_log_path),
+            "--tensorboard_log_path",
+            str(tensorboard_log_path),
             "--verbose",
             "0",
             "--no_progress_bar",
+            "--total_timesteps",
+            "100000",
         ]
     )
-    data_path = Path("data")
-    data_path.mkdir(exist_ok=True)
 
-    hidden_sizes = np.arange(1, 256 + 1, 1)
+    hidden_sizes = [32]
+    depths = [2]
+    dofs = [3, 4, 7]
+    hyperparameters = list(itertools.product(hidden_sizes, depths, dofs))
+    data_paths = []
 
     with ProcessPoolExecutor() as executor:
         futures = {}
-        for i, hidden_size in enumerate(hidden_sizes):
-            args = deepcopy(args)
-            args.hidden_size = hidden_size
-            future = executor.submit(train, args)
-            futures[future] = i
+        for hidden_size, depth, dof in hyperparameters:
+            for i in range(repeats):
+                args = deepcopy(args)
+                args.hidden_size = hidden_size
+                args.depth = depth
+                args.dof = dof
+                name = f"{dof}_" + "_".join([str(hidden_size)] * depth) + f"/{i}"
+                args.model_path = str(model_path / name)
+                args.eval_log_path = str(eval_log_path / name)
+                args.tensorboard_log_path = str(tensorboard_log_path / name)
+                future = executor.submit(train, args)
+                futures[future] = args
 
         for future in tqdm(as_completed(futures), total=len(futures)):
             num_params = future.result()
-            i = futures[future]
-            hidden_size = hidden_sizes[i]
-            evaluations_log_path = (
-                str(data_path / args.model_path)
-                + f"_{hidden_size}/evaluations.npz"
-            )
-            data = np.load(evaluations_log_path)
+            args = futures[future]
+            _eval_log_path = Path(args.eval_log_path) / "evaluations.npz"
+            data = np.load(_eval_log_path)
             data = dict(data)
-            data["hidden_size"] = hidden_size
+            data["hidden_size"] = args.hidden_size
+            data["depth"] = args.depth
             data["num_params"] = num_params
-            np.savez(evaluations_log_path, **data)
+            data["dof"] = args.dof
+            np.savez(_eval_log_path, **data)
+            data_paths.append(_eval_log_path)
+
+    combine_data(experiment, data_paths, eval_log_path)
 
 
 if __name__ == "__main__":
--- control/src/rl/train.py
+++ control/src/rl/train.py
@@ -1,26 +1,24 @@
 import argparse
-import itertools
 import os
 
 import custom_policy
 import gymnasium as gym
 import numpy as np
 from stable_baselines3 import A2C, DDPG, PPO, SAC, TD3, HerReplayBuffer
-from stable_baselines3.common.callbacks import (
-    EvalCallback
-)
+from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.noise import NormalActionNoise, VectorizedActionNoise
-from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.her.goal_selection_strategy import GoalSelectionStrategy
-from tqdm import tqdm
 
 ONE_ENV_ALGS = {DDPG, TD3, SAC}
 
 
 def parse_args(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument("model_path", type=str, help="Path to save model as")
+    parser.add_argument("--model_path", type=str, help="Path to save model as")
+    parser.add_argument("--eval_log_path", type=str, help="Path to save evaluation logs as")
+    parser.add_argument("--tensorboard_log_path", type=str, help="Path to save tensorboard logs as")
     parser.add_argument(
         "--total_timesteps",
         type=int,
@@ -36,7 +34,7 @@ def parse_args(argv=None):
     parser.add_argument(
         "--n_eval_episodes",
         type=int,
-        default=1000,
+        default=100,
         help="The number of episodes to test the agent",
     )
     parser.add_argument(
@@ -49,18 +47,10 @@ def parse_args(argv=None):
     parser.add_argument(
         "--dof",
         type=int,
-        default=3,
+        default=4,
         help="The number of degrees of freedom of the robot",
     )
-    parser.add_argument(
-        "--repeat",
-        type=int,
-        default=1,
-        help="Number of times to repeat training",
-    )
-    parser.add_argument(
-        "--std", type=float, default=0.1, help="Scale of the noise"
-    )
+    parser.add_argument("--std", type=float, default=0.1, help="Scale of the noise")
     parser.add_argument(
         "--hidden_size",
         type=int,
@@ -68,85 +58,91 @@ def parse_args(argv=None):
         help="Number of hidden units in the policy network",
     )
     parser.add_argument(
-        '--verbose',
+        "--depth",
         type=int,
         default=1,
-        help='Verbose level',
+        help="Number of hidden layers in the policy network",
     )
     parser.add_argument(
-        '--no_progress_bar',
-        action='store_true',
-        help='Do not display a progress bar using tqdm and rich',
+        "--learning_rate",
+        type=float,
+        default=1e-3,
+        help="learning rate for adam optimizer, the same learning rate will be used for all networks (Q-Values, Actor and Value function) it can be a function of the current progress remaining (from 1 to 0)",
+    )
+    parser.add_argument(
+        "--verbose",
+        type=int,
+        default=1,
+        help="Verbose level",
+    )
+    parser.add_argument(
+        "--no_progress_bar",
+        action="store_true",
+        help="Do not display a progress bar using tqdm and rich",
     )
     args = parser.parse_args(argv)
-    reward_type = "Dense" if args.reward_type == "Dense" else ""
-    dof = "3DOF" if args.dof == 3 else ""
-    args.env_id = f"WAMVisualReach{reward_type}{dof}-v2"
     return args
 
 
 def train(args):
-    env = gym.make(args.env_id)
+    reward_type = "Dense" if args.reward_type == "Dense" else ""
+    if args.dof < 7:
+        dof = f"{args.dof}DOF"
+    dof = "3DOF" if args.dof == 3 else ""
+    env_id = f"WAMVisualReach{reward_type}{dof}-v2"
+
+    env = gym.make(env_id)
     n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(
-        mean=np.zeros(n_actions), sigma=args.std * np.ones(n_actions)
-    )
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=args.std * np.ones(n_actions))
     alg = TD3
-    for _ in range(args.repeat):
-        n_envs = 1 if alg in ONE_ENV_ALGS else os.cpu_count()
-        vec_action_noise = (
-                action_noise
-                if alg in ONE_ENV_ALGS
-                else VectorizedActionNoise(action_noise, n_envs=n_envs)
-            )
-        vec_env = make_vec_env(
-            args.env_id, n_envs=n_envs, vec_env_cls=DummyVecEnv
-        )
-        model = alg(
-            "CustomMultiInputPolicy",
-            vec_env,
-            # verbose=1,
-            # batch_size=512,
-            tensorboard_log=f"./logs/{args.model_path}_{args.hidden_size}/",
-            # replay_buffer_class=HerReplayBuffer,
-            # # Parameters for HER
-            # replay_buffer_kwargs=dict(
-            #     n_sampled_goal=4,
-            #     goal_selection_strategy=goal_selection_strategy,
-            # ),
-            action_noise=vec_action_noise,
-            policy_kwargs=dict(
-                net_arch=dict(
-                    pi=[args.hidden_size],
-                    qf=[args.hidden_size],
-                ),
-                # optimizer_class=torch.optim.AdamW,
-                share_features_extractor=True,
+    n_envs = 1 if alg in ONE_ENV_ALGS else os.cpu_count()
+    vec_action_noise = action_noise if alg in ONE_ENV_ALGS else VectorizedActionNoise(action_noise, n_envs=n_envs)
+    vec_env = make_vec_env(env_id, n_envs=n_envs, vec_env_cls=DummyVecEnv)
+    model = alg(
+        "CustomMultiInputPolicy",
+        vec_env,
+        verbose=args.verbose,
+        tensorboard_log=args.tensorboard_log_path,
+        # replay_buffer_class=HerReplayBuffer,
+        # # Parameters for HER
+        # replay_buffer_kwargs=dict(
+        #     n_sampled_goal=4,
+        #     goal_selection_strategy=goal_selection_strategy,
+        # ),
+        action_noise=vec_action_noise,
+        policy_kwargs=dict(
+            net_arch=dict(
+                pi=[args.hidden_size] * args.depth,
+                qf=[args.hidden_size] * args.depth,
             ),
-        )
-        if args.verbose >= 1:
-            print(model.policy)
-        num_params = sum(p.numel() for p in model.policy.parameters())
-        param_str = ""
-        if num_params > 1e6:
-            param_str = f"{num_params / 1e6:.2f} M parameters"
-        else:
-            param_str = f"{num_params} parameters"
-        if args.verbose >= 1:
-            print(param_str)
-        eval_callback = EvalCallback(
-            vec_env,
-            n_eval_episodes=args.n_eval_episodes,
-            eval_freq=args.eval_freq,
-            log_path=f"./data/{args.model_path}_{args.hidden_size}/",
-            verbose=args.verbose,
-            best_model_save_path=args.model_path,
-        )
-        model.learn(
-            total_timesteps=args.total_timesteps,
-            callback=eval_callback,
-            progress_bar=not args.no_progress_bar,
-        )
+            # optimizer_class=torch.optim.AdamW,
+            share_features_extractor=True,
+        ),
+        learning_rate=args.learning_rate,
+    )
+    if args.verbose >= 1:
+        print(model.policy)
+    num_params = sum(p.numel() for p in model.policy.parameters())
+    param_str = ""
+    if num_params > 1e6:
+        param_str = f"{num_params / 1e6:.2f} M parameters"
+    else:
+        param_str = f"{num_params} parameters"
+    if args.verbose >= 1:
+        print(param_str)
+    eval_callback = EvalCallback(
+        vec_env,
+        n_eval_episodes=args.n_eval_episodes,
+        eval_freq=args.eval_freq,
+        log_path=args.eval_log_path,
+        verbose=args.verbose,
+        best_model_save_path=args.model_path,
+    )
+    model.learn(
+        total_timesteps=args.total_timesteps,
+        callback=eval_callback,
+        progress_bar=not args.no_progress_bar,
+    )
     return num_params
 
 
